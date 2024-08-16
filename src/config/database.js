bafb63d9920d2f568201e77562d36bcd5e80b904
Ian <iansrc064@gmail.com>
2024-08-16 19:41:46 -0300

    Mudei a estrutura do projeto e adiconei o .env.sample

--- src/config/database.js
+++ src/config/database.js
@@ -3,3 +3,14 @@
 // module.exports = {
 //   uri: 'postgresql://'+USUARIO+':'+SENHA+'@'+HOST+':'+PORTA_BANCO+'/'+BANCO
 // }
+
+const mysql = require('mysql2')
+
+const connection = mysql;mysql.createPool({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+}) // usando process.env para puxar informações do arquivo .env
+
+module.exports = connection
