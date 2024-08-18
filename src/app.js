52c72a8dd44c8b563356f950fd701987c0a4e8bc
z4nt <antonielf1203@gmail.com>
2024-08-18 01:01:26 -0300

    ta funcionado 50%

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
+var cors = require('cors')
+const bodyParser = require('body-parser');
+
 
 const app = express();
 
 require('dotenv').config()
 
-require('./routes/userRoutes')
+const user = require('./routes/userRoutes')
 require('./routes/productRoutes')
 require('./routes/categoryRoutes')
-
+app.use(cors())
+app.use(bodyParser.json());
 const PORT = process.env.PORTA_BANCO || 10000
 
 app.listen(PORT, ()=> { console.log('http://localhost:'+PORT)})
--- src/routes/app-express.js
+++ src/routes/app-express.js
@@ -1,9 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require('express');
 var cors = require('cors')
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const app = express();
 
-app.use(bodyParser.json()) 
-app.use(cors()) 
+require('dotenv').config()
+
+const user = require('./userRoutes')
+// require('./routes/productRoutes')
+// require('./routes/categoryRoutes')
+ 
+const PORT = process.env.PORTA_BANCO || 8888
+app.use(cors())
+app.use(bodyParser.json());
+app.listen(PORT, ()=> { console.log('http://localhost:'+PORT)})
 
 module.exports = app
